0a7c6f910331f3dac3584e66cfd51b17978b30e9
Lucas dos Santos Amorim Rego <lucass235@gmail.com>
2020-10-06 20:15:19 -0300

    atvs de matriz

---
+++ matrizMagica/src/matrizmagica/MatrizMagica.java
@@ -0,0 +1,17 @@
+
+package matrizmagica;
+
+/**
+ *
+ * @author lucas
+ */
+public class MatrizMagica {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
